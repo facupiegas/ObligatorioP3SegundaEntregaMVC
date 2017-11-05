e91cac93779917b7cef8fee5d51f22c7143d61f6
Nicolas Muniz <nicolasmuniz7@gmail.com>
2017-11-05 18:14:03 -0300

    Calificar Proveedor funcionando(?)
    Parece que funciona..

---
+++ ObligatorioP3MVC/ViewModels/CrearCalificacionViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using ObligatorioP3MVC.Models;
+
+namespace ObligatorioP3MVC.ViewModels
+{
+    public class CrearCalificacionViewModel
+    {
+        public CalificacionProveedor CalificacionProveedor { get; set; }
+        public ServicioContratado ServicioContratado { get; set; }
+    }
+}
