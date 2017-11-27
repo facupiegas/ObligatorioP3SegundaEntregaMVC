a11a6769fa7665dbab02f6ee9417790dc27684bc
Nicolas Muniz <nicolasmuniz7@gmail.com>
2017-11-27 12:58:41 -0300

    Fix en Calificar Proveedor
    Daba error al intentar de calificar un proveedor ya calificado.

---
+++ ObligatorioP3MVC/Migrations/201711271510089_emailaddress_Organizador.cs
@@ -0,0 +1,16 @@
+namespace ObligatorioP3MVC.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class emailaddress_Organizador : DbMigration
+    {
+        public override void Up()
+        {
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
